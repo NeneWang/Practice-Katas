001d2e7fade7a9723037792578bfdb1dd5ac2308
NelsonWang <wangnelson2@gmail.com>
2021-02-25 10:42:25 -0500

    D20 Highest and Lowest | Start

--- Java/src/D19.java
+++ Java/src/D19.java
@@ -2,44 +2,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class D19 {
-    public static void main(String[] args) {
-    	System.out.println(accum("ZpglnRxqenU"));
-    	
-    	
-    }
-    
-    
-    public static String accum(String s) {
-    	ArrayList<String> accumArr = new ArrayList<>(Arrays.asList(s.split("")));
-    	String stringToReturn = "";
-    	
-    	
-    	
-    	for(int indexAcumArr=0; indexAcumArr< accumArr.size(); indexAcumArr++) {
-    		String appendThis = accumArr.get(indexAcumArr).toUpperCase();
-    		stringToReturn+=appendThis;
-    		appendThis = appendThis.toLowerCase();
-    		for(int repeatXTimes = 0; repeatXTimes < indexAcumArr; repeatXTimes++) {
-    			
-    			stringToReturn+=appendThis;
-    		}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(accum("ZpglnRxqenUss"));
 
-    		stringToReturn+="-";
-    	}
-    	
-        return stringToReturn.substring(0, stringToReturn.length()-1);
-    	
-    }
-    
-        
-        public static String accumSimple(String s) {
-            String result = "";
-            for(int i = 0; i != s.length(); i++)
-            {
-                for(int j = 1; j != i+2; j++)
-                    result = result + (j == 1 ? s.toUpperCase().charAt(i) : s.toLowerCase().charAt(i));
-                result = result + (i != s.length()-1 ? '-' : "");
-            }
-            return result;
-        }
+	}
+
+	public static String accum(String s) {
+		ArrayList<String> accumArr = new ArrayList<>(Arrays.asList(s.split("")));
+		String stringToReturn = "";
+
+		for (int indexAcumArr = 0; indexAcumArr < accumArr.size(); indexAcumArr++) {
+			String appendThis = accumArr.get(indexAcumArr).toUpperCase();
+			stringToReturn += appendThis;
+			appendThis = appendThis.toLowerCase();
+			for (int repeatXTimes = 0; repeatXTimes < indexAcumArr; repeatXTimes++) {
+				stringToReturn += appendThis;
+			}
+
+			stringToReturn += "-";
+		}
+
+		return stringToReturn.substring(0, stringToReturn.length() - 1);
+
+	}
+
+	public static String accumSimple(String s) {
+		String result = "";
+		for (int i = 0; i != s.length(); i++) {
+			for (int j = 1; j != i + 2; j++)
+				result = result + (j == 1 ? s.toUpperCase().charAt(i) : s.toLowerCase().charAt(i));
+			result = result + (i != s.length() - 1 ? '-' : "");
+		}
+		return result;
+	}
 }
--- Java/src/D20.java
+++ Java/src/D20.java
@@ -1,9 +1,42 @@
 
+import java.util.*;
+
+//https://www.codewars.com/kata/554b4ac871d6813a03000035/train/java
 public class D20 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(
+		highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+	}
 
+	public static String highAndLow(String numbers) {
+		// Code here or
+		ArrayList<String> accumArr = new ArrayList<>(Arrays.asList(numbers.split(" ")));
+		List<Integer> numbersArr = new ArrayList<>();
+		Iterator it = accumArr.iterator();
+
+		if(accumArr.size()<1) {
+			return "Error";
+		}
+		
+
+		int max = Integer.parseInt(accumArr.get(0)), min = max;
+		while( it.hasNext() ) {
+			int element = Integer.parseInt((String)it.next());
+			if(element>max) {
+				max = element;
+			}
+			if(element<min) {
+				min = element;
+			}
+		}
+		
+		
+		
+		
+		
+		System.out.println(numbersArr);
+		return String.format("%d %d", max, min);
 	}
 
 }
