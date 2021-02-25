505bb46045538547b56276ea3c6fdd8e1c5cc34b
NelsonWang <wangnelson2@gmail.com>
2021-02-24 22:28:58 -0500

    D19 Mumbling | Simple

--- Java/src/D19.java
+++ Java/src/D19.java
@@ -30,4 +30,16 @@ public class D19 {
         return stringToReturn.substring(0, stringToReturn.length()-1);
     	
     }
+    
+        
+        public static String accumSimple(String s) {
+            String result = "";
+            for(int i = 0; i != s.length(); i++)
+            {
+                for(int j = 1; j != i+2; j++)
+                    result = result + (j == 1 ? s.toUpperCase().charAt(i) : s.toLowerCase().charAt(i));
+                result = result + (i != s.length()-1 ? '-' : "");
+            }
+            return result;
+        }
 }
