b0d7cab8e11ecab1af2ce43424bf449ba73fbc4d
NelsonWang <wangnelson2@gmail.com>
2021-02-24 21:54:33 -0500

    D18 | refactor

---
+++ Java/src/D18.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class D18 {
+	
+//	https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/java
+    public static void main(String[] args) {
+        // TODO code application logic here
+    	System.out.println(getCount("Hello"));
+    }
+    
+    public static int getCount(String str) {
+        int vowelsCount = 0;
+
+        ArrayList<String> vowels = new ArrayList<>(Arrays.asList("A", "E", "I", "O", "U", "a", "e", "i", "o", "u"));
+        ArrayList<String> retainedVowels = new ArrayList<>(Arrays.asList(str.split("")));
+        // your code here
+        System.out.println(retainedVowels);
+        retainedVowels.retainAll(vowels);
+
+        System.out.println(retainedVowels);
+        return retainedVowels.size();
+      }
+}
