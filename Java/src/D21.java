9d0d03a121e85e6acc2103196d87af97a6a0608a
NelsonWang <wangnelson2@gmail.com>
2021-02-26 18:12:18 -0500

    D21 | Basic

--- Java/src/D21.java
+++ Java/src/D21.java
@@ -1,9 +1,19 @@
-
+import java.util.ArrayList;
+import java.util.Arrays;
+//https://www.codewars.com/kata/5715eaedb436cf5606000381/train/java
 public class D21 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(sum(new int[] { 1, 2, 3, 4, 5 }));
+	}
 
+	public static int sum(int[] arr) {
+		int sum = 0;
+		for (int num : arr) {
+			sum += num > 0 ? num : 0;
+		}
+
+		return sum;
 	}
 
 }
