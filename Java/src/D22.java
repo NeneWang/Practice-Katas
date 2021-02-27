9e60cb78a705e08bfb878cfb32a2b6a3b585ba8b
NelsonWang <wangnelson2@gmail.com>
2021-02-27 17:05:27 -0500

    D22 return truefalse | B

--- Java/src/D22.java
+++ Java/src/D22.java
@@ -3,7 +3,11 @@ public class D22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(convert(true));
+	}
 
+	public static String convert(boolean b) {
+		return b == true ? "true" : "false";
 	}
 
 }
